5730865d92fa0710b2958aea9e2e8fa1810ee7e8
BlackcoinDev <dev@blackcoin.dev>
2024-09-20 21:19:44 +0200

    testnet: segwit Fri Sep 20 2024 18:00:00 GMT+0000

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -229,7 +229,7 @@ public:
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1726790400; // Wed Sep 20 2024 00:00:00 GMT+0000
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1726862400; // Fri Sep 20 2024 20:00:00 GMT+0000
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].min_activation_height = 0; // No activation delay
 

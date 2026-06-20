af8d4c108bc4e4750c6dde01f3efa99df81dc2cf
michelvankessel <michelvankessel@protonmail.com>
2026-06-20 11:26:17 +0200

    qt: lower MAX_BLOCK_TIME_GAP to reflect BLK block time
    Maximum gap between node time and block time used for the "Catching up..." mode in GUI.

--- src/chain.h
+++ src/chain.h
@@ -43,7 +43,7 @@ static constexpr int64_t TIMESTAMP_WINDOW = MAX_FUTURE_BLOCK_TIME;
  *
  * Ref: https://github.com/bitcoin/bitcoin/pull/1026
  */
-static constexpr int64_t MAX_BLOCK_TIME_GAP = 90 * 60;
+static constexpr int64_t MAX_BLOCK_TIME_GAP = 30 * 60;
 
 class CBlockFileInfo
 {
